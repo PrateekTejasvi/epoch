48b49ab998b1f8c0469838526b7662c6a623c341
PrateekTejasvi <prateek.tejasvi@gmail.com>
2026-03-14 10:24:15 +0530

    added requirements and config file

---
+++ config.py
@@ -0,0 +1,16 @@
+TARGET = "Addicted_Score"
+
+CLUSTER_FEATURES = [
+    "Avg_Daily_Usage_Hours",
+    "Sleep_Hours_Per_Night",
+    "Mental_Health_Score",
+    "Conflicts_Over_Social_Media",
+    "Addicted_Score"
+]
+
+HARM_FEATURES = [
+    "Mental_Health_Score",
+    "Sleep_Hours_Per_Night",
+    "Conflicts_Over_Social_Media",
+    "Addicted_Score"
+]
